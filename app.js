30e0ed9df8ed606ba7e770a3423e9bb0a5091c39
Dun <kathymaidung@gmail.com>
2020-08-18 16:19:26 +0300

    Passport

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const mongoose = require('mongoose');
 
+var passport = require('passport');
+var authenticate = require('./authenticate');
+
 const Dishes = require('./models/dishes');
 const Promotions = require('./models/promotions');
 const Leaders = require('./models/leaders');
@@ -34,26 +37,22 @@ app.use(session({
   store: new FileStore()
 }));
 
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 function auth (req,res,next) {
-  console.log(req.session);
+  console.log(req.user);
 
-  if (!req.session.user) {
+  if (!req.user) {
       var err = new Error('You are not authenticated!');
       err.status = 403;
-      return next(err);
+      next(err);
   }
-  else {
-    if(req.session.user === 'authenticated') {
+  else {  
       next();
-    }
-    else {
-      var err = new Error('You are not authenticated!');
-      err.status = 403;
-      return next(err);
-    }
   }
 }
 
